7633d311686ee0d5791d2bdf3ac14a1f1fd35e71
jiyoulee <55631731+jiyoulee@users.noreply.github.com>
2023-03-27 22:35:32 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+using namespace std;
+
+int L;
+
+int main() {
+    /*
+     * Input.
+     */
+    scanf("%d", &L);
+
+    /*
+     * Output.
+     */
+    printf("%d", (L + 4) / 5);
+
+    return 0;
+}
