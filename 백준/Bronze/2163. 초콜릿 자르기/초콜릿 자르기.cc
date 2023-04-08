713f8c612288d5fbe2fabca5936b6925cf94e199
jiyoulee <55631731+jiyoulee@users.noreply.github.com>
2023-04-08 22:52:53 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+using namespace std;
+
+int N, M;
+
+int main() {
+    /*
+     * Input.
+     */
+    scanf("%d%d", &N, &M);
+    
+    /*
+     * Output.
+     */
+    printf("%d", N * M - 1);
+    
+    return 0;
+}
