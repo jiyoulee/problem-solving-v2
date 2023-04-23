d404162a9f4a8c8668222bb5e96a0c7f9711ff9c
jiyoulee <55631731+jiyoulee@users.noreply.github.com>
2023-04-23 23:24:35 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+using namespace std;
+
+int A, B;
+
+int main() {
+    /*
+     * Input.
+     */
+    scanf("%d%d", &A, &B);
+    
+    /*
+     * Output.
+     */
+    printf("%d", A * B);
+    
+    return 0;
+}
