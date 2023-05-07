7929480a7ea5dc3ae3611f4292d20ed6eafbcf9e
jiyoulee <55631731+jiyoulee@users.noreply.github.com>
2023-05-07 19:33:50 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+using namespace std;
+
+int Y;
+
+int main() {
+    /*
+     * Input.
+     */
+    scanf("%d", &Y);
+    
+    /*
+     * Output.
+     */
+    printf("%d", Y - 543);
+    
+    return 0;
+}
