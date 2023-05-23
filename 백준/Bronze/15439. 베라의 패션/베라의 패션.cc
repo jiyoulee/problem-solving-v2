f947b66b365b6645565ca4b92fb9bc00ea8d26ad
jiyoulee <55631731+jiyoulee@users.noreply.github.com>
2023-05-24 01:58:05 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+using namespace std;
+
+int N;
+
+int main() {
+    /*
+     * Input.
+     */
+    scanf("%d", &N);
+    
+    /*
+     * Output.
+     */
+    printf("%d", N * (N - 1));
+    
+    return 0;
+}
